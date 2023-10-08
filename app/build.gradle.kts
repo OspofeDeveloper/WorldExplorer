8f109ca2ee626a2a76b2dbbd0a375f0e551aeffc
Opozo2 <ospofe.developer02@gmail.com>
2023-10-08 18:52:50 +0200

    Dagger - Hilt setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,15 @@ android {
 dependencies {
 
     val navVersion = "2.7.3"
+    val daggerHiltVersion = "2.48"
 
     //Navigation Components
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     //Dagger-Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerHiltVersion")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/worldexplorer/MainActivity.kt
+++ app/src/main/java/com/example/worldexplorer/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.worldexplorer.ui
+package com.example.worldexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.worldexplorer.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

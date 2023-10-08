aaeb4fd085dfb1d6d97f36496c1d9cea7a84a541
Opozo2 <ospofe.developer02@gmail.com>
2023-10-09 00:50:35 +0200

    Order By Dropdown Menu interface implemented and Recyclerview interface updated

--- app/src/main/java/com/example/worldexplorer/ui/listCountriesScreen/view/ListCountriesFragment.kt
+++ app/src/main/java/com/example/worldexplorer/ui/listCountriesScreen/view/ListCountriesFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.worldexplorer.R
 import com.example.worldexplorer.databinding.FragmentListCountriesBinding
 import com.example.worldexplorer.domain.model.CountryItem
 import com.example.worldexplorer.ui.listCountriesScreen.viewmodel.CountryViewModel
@@ -19,12 +22,21 @@ class ListCountriesFragment : Fragment() {
     private var _binding: FragmentListCountriesBinding? = null
     private val binding get() = _binding!!
 
+    //If we put this code in onCreateView function we have problems when returning to this fragment from navigation
+    override fun onResume() {
+        super.onResume()
+        val orderByItems = resources.getStringArray(R.array.order_by_dropdown_options)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.order_by_dropdown_menu_item, orderByItems)
+        binding.autoCompleteText.setAdapter(arrayAdapter)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentListCountriesBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 

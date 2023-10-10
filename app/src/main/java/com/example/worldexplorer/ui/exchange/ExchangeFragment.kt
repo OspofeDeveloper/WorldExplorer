6adbae98177c37780c278594d7891b862391333d
Opozo2 <ospofe.developer02@gmail.com>
2023-10-10 13:20:42 +0200

    Navigation implemented

--- app/src/main/java/com/example/worldexplorer/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/example/worldexplorer/ui/exchange/ExchangeFragment.kt
@@ -1,21 +1,21 @@
-package com.example.worldexplorer.ui.converter
+package com.example.worldexplorer.ui.exchange
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.worldexplorer.databinding.FragmentConverterBinding
+import com.example.worldexplorer.databinding.FragmentExchangeBinding
 
-class ConverterFragment : Fragment() {
+class ExchangeFragment : Fragment() {
 
-    private var _binding: FragmentConverterBinding? = null
+    private var _binding: FragmentExchangeBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentConverterBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentExchangeBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 }

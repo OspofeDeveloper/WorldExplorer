a4aea4283a8f3c79be16b5446550b83391828868
Opozo2 <ospofe.developer02@gmail.com>
2023-10-10 12:38:45 +0200

    App first structure implemented

---
+++ app/src/main/java/com/example/worldexplorer/ui/countries/CountriesFragment.kt
@@ -0,0 +1,24 @@
+package com.example.worldexplorer.ui.countries
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.worldexplorer.databinding.FragmentCountriesBinding
+
+// TODO: Rename parameter arguments, choose names that match
+
+class CountriesFragment : Fragment() {
+
+    private var _binding: FragmentCountriesBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCountriesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/worldexplorer/ui/quiz/QuizFragment.kt
@@ -0,0 +1,22 @@
+package com.example.worldexplorer.ui.quiz
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.worldexplorer.databinding.FragmentQuizBinding
+
+class QuizFragment : Fragment() {
+
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQuizBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}

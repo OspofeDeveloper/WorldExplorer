8cd944a3d64262612fed92dfc35403d00df7cf10
Opozo2 <ospofe.developer02@gmail.com>
2023-10-11 19:23:52 +0200

    Screens Merged

--- app/src/main/java/com/example/worldexplorer/ui/countries/CountriesFragment.kt
+++ app/src/main/java/com/example/worldexplorer/ui/countries/CountriesFragment.kt
@@ -1,6 +1,7 @@
 package com.example.worldexplorer.ui.countries
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
